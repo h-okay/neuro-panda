1b0fc74f089f6d51ef3f028213242283ca6c6fb1
Hakan Okay <hokay18@gmail.com>
2023-08-03 13:11:02 -0400

    Updated Neuron tests

--- tests/test_neuron.py
+++ tests/test_neuron.py
@@ -1,32 +1,23 @@
-import numpy as np
-
-from src.functions.sigmoid import sigmoid
+from src.functions.activation import relu, sigmoid, tanh
 from src.network.neuron import Neuron
 
 
-def test_feedforward_with_zero_weights_and_zero_bias():
-    neuron = Neuron(np.zeros(3), 0.0)
-    inputs = np.array([1.0, 2.0, 3.0])
-    result = neuron.feedforward(inputs)
-    assert result == sigmoid(0.0)
+def test_feedforward_sigmoid():
+    neuron = Neuron([0.5, 0.5], -1)
+    input_data = [2, 3]
+    expected_output = 0.8175744761936437
+    assert expected_output == neuron.feedforward(input_data, sigmoid)
 
 
-def test_feedforward_with_nonzero_weights_and_zero_bias():
-    neuron = Neuron(np.array([0.5, 0.5]), 0.0)
-    inputs = np.array([1.0, 1.0])
-    result = neuron.feedforward(inputs)
-    assert result == sigmoid(1)
+def test_feedforward_relu():
+    neuron = Neuron([1, -1], 0)
+    input_data = [10, 5]
+    expected_output = 5
+    assert expected_output == neuron.feedforward(input_data, relu)
 
 
-def test_feedforward_with_zero_weights_and_nonzero_bias():
-    neuron = Neuron(np.zeros(2), 0.5)
-    inputs = np.array([2.0, 3.0])
-    result = neuron.feedforward(inputs)
-    assert result == sigmoid(0.5)
-
-
-def test_feedforward_with_nonzero_weights_and_nonzero_bias():
-    neuron = Neuron(np.array([2.0, 3.0]), 1.0)
-    inputs = np.array([0.5, 1.0])
-    result = neuron.feedforward(inputs)
-    assert result == sigmoid(5)
+def test_feedforward_tanh():
+    neuron = Neuron([0.2, 0.3], 0.1)
+    input_data = [1, 1]
+    expected_output = 0.5370495669980352
+    assert expected_output == neuron.feedforward(input_data, tanh)
